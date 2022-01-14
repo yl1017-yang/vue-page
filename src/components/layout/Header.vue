922725e57de674ade4f663ee1131b29677be1392
yanglae <yanglae@hanmail.net>
2022-01-14 17:24:51 +0900

    header

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
         상단네비바 테스트
+
+        <button class="c-Button c-Button--close">X</button>
     </div>
 </template>
 <script>
@@ -8,3 +10,14 @@ export default {
     name: "header",
 }
 </script>
+
+<style>
+.c-Button {
+  border: none;
+  border-radius: 2px;
+}
+
+.c-Button--close {
+  background-color: red;
+}
+</style>
