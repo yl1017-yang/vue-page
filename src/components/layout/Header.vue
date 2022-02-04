b036e39321ee9e6a41becab366e0d660170656c0
yanglae <yanglae@hanmail.net>
2022-02-04 17:22:12 +0900

    header

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,16 +1,22 @@
 <template>
     
-  <v-app-bar app>
-    <header>
-      <h1>YANG</h1>
-      <nav>
-        <ul>
-          <li><router-link to="/vue-page/">HOME</router-link></li>
-          <li><router-link to="/vue-page/views/about">ABOUT</router-link></li>
-        </ul>
-      </nav>
-    </header>
+  <v-app-bar
+    absolute
+    color="#444"
+    dark
+    flat
+    fixed
+    height="50"
+  >
+    <v-app-bar-title>YANG</v-app-bar-title>
+    <template v-slot:extension>
+      <v-tabs align-with-title>
+        <v-tab><router-link to="/vue-page/">HOME</router-link></v-tab>
+        <v-tab><router-link to="/vue-page/views/about">ABOUT</router-link></v-tab>
+      </v-tabs>
+    </template>
   </v-app-bar>
+
     
 </template>
 
